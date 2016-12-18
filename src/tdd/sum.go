4a6aac2b5ebc852e46763bcc65a09b79a0baa139
THAWATCHAI SINGNGAM <34d30y@gmail.com>
2016-12-18 19:26:11 +0700

    change to one package

---
+++ src/tdd/sum.go
@@ -0,0 +1,6 @@
+package tdd
+
+
+func Sum(a, b int) int{
+	return a + b
+}
---
+++ src/tdd/sum_test.go
@@ -0,0 +1,10 @@
+package tdd
+
+import "testing"
+
+func TestSum(t *testing.T) {
+	result := Sum(1, 2)
+	if result != 3 {
+		t.Error("Expect 3, but got ", result)
+	}
+}
